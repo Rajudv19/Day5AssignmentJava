44e3d6708aa962daae281d2bd5aa41db48a53855
Rajudv19 <rajudv1997@gmail.com>
2022-04-19 00:36:46 +0530

    VowelConsonant Code

---
+++ VowelConsonant.java
@@ -0,0 +1,18 @@
+package com.BridgeLabs.day5;
+
+public class VowelConsonant 
+{
+	public static void main(String[] args) 
+	{	
+		char ch = 'r';
+	
+		if(ch == 'a' || ch == 'e'  || ch == 'i' || ch == 'o' || ch == 'u')
+		{
+			System.out.println("The given variable is Vowel");
+		}
+		else
+		{
+			System.out.println("The given variable is consonant");
+		}
+	}
+}
