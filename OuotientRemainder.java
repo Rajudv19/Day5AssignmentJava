3bb6db4832d53d6d15c684e7ce0a088f0e464b53
Rajudv19 <rajudv1997@gmail.com>
2022-04-19 00:34:59 +0530

    OuotientRemainder Code

---
+++ OuotientRemainder.java
@@ -0,0 +1,21 @@
+package com.BridgeLabs.day5;
+import java.util.Scanner;
+public class OuotientRemainder 
+{
+	public static void main (String[] args)
+	{
+		int dividend,divisor;
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter the value of dividend : ");
+		int Dividend = reader.nextInt();
+		System.out.println("Enter the value of divisor : ");
+		int Divisor  = reader.nextInt();
+		
+		int Quotient  = Dividend/Divisor;
+		int Remainder = Dividend%Divisor;
+		
+		System.out.println("Qoutient  : " + Quotient);
+		System.out.println("Remainder : " + Remainder);
+	}
+}
